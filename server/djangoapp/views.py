40dca9a603098c786b6168af17c9ea7996d24ba3
rafael Pietsch <rafaelpietsch@outlook.com>
2025-09-17 19:52:03 -0400

    Added review dealers page and review forms to submit reviews

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -98,17 +98,17 @@ def get_dealerships(request, state="All"):
     dealerships = get_request(endpoint)
     return JsonResponse({"status":200,"dealers":dealerships})
 
-def get_dealer_reviews(request, dealer_id):
+def get_dealer_details(request, dealer_id):
     if(dealer_id):
-        endpoint = "/fetchReviews/dealer/" + str(dealer_id)
+        endpoint = "/fetchDealer/"+str(dealer_id)
         dealership = get_request(endpoint)
         return JsonResponse({"status":200,"dealer":dealership})
     else:
-        return JsonResponse({"status": 400, "message": "Bad Request"})
+        return JsonResponse({"status":400,"message":"Bad Request"})
 
-def get_dealer_details(request, dealer_id):
+def get_dealer_reviews(request, dealer_id):
     if(dealer_id):
-        endpoint = "/fetchReviews/dealer/" + str(dealer_id)
+        endpoint = "/fetchReviews/dealer/"+str(dealer_id)
         reviews = get_request(endpoint)
         for review_detail in reviews:
             response = analyze_review_sentiments(review_detail['review'])
@@ -116,9 +116,7 @@ def get_dealer_details(request, dealer_id):
             review_detail['sentiment'] = response['sentiment']
         return JsonResponse({"status":200,"reviews":reviews})
     else:
-        return JsonResponse({"status": 400, "message": "Bad Request"})
-
-
+        return JsonResponse({"status":400,"message":"Bad Request"})
 
 def add_review(request):
     if(request.user.is_anonymous == False):
