ff5f419528ee3214132b1f5a6fd89f1556981754
rafael Pietsch <rafaelpietsch@outlook.com>
2025-09-17 18:47:31 -0400

    Added CarModel and CarMake models to Django app and commited modified files of login, logout and registration

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,25 +1,51 @@
-# Uncomment the following imports before adding the Model code
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    country = models.TextField()
+    foundedYear = models.IntegerField(default=2025,
+        validators=[
+            MaxValueValidator(2026),
+            MinValueValidator(1900)
+        ])
+
+    def __str__(self):
+        return self.name  # Return the name as the string representation
 
 
-# Create your models here.
-
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
-
-
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    name = models.CharField(max_length=100)
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        ('COUPE', 'Coupe'),
+        ('PICKUP', 'Pick-up'),
+        ('HATCH', 'Hatchback'),
+    ]
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+    year = models.IntegerField(default=2023,
+        validators=[
+            MaxValueValidator(2026),
+            MinValueValidator(2005)
+        ])
+    top_speed = models.IntegerField(default=100,
+        validators=[
+            MaxValueValidator(400),
+            MinValueValidator(50)
+        ])
+    FUEL_TYPES = [
+        ('GASOLINE', 'Gasoline'),
+        ('ELECTRIC', 'Electric'),
+        ('HYBRID', 'Hybrid'),
+        ('DIESEL', 'Diesel'),
+        ('FLEX', 'Flex'),
+    ]
+    fuel_type = models.CharField(max_length=10, choices=FUEL_TYPES, default='GASOLINE')
+    
+    def __str__(self):
+        return self.name  # Return the name as the string representation
