0a8fec2fbd26adb74f555bb3dc4ff5d3230f4fcc
rafael Pietsch <rafaelpietsch@outlook.com>
2025-09-17 20:59:24 -0400

    Corrected lint messages

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -7,9 +6,11 @@ class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     country = models.TextField()
-    foundedYear = models.IntegerField(default=2025, 
-                                      validators=[MaxValueValidator(2026),MinValueValidator(1900)]
-                                    )
+    foundedYear = models.IntegerField(default=2025,
+                                      validators=[
+                                        MaxValueValidator(2026), 
+                                        MinValueValidator(1900)
+                                    ])
 
     def __str__(self):
         return self.name  # Return the name as the string representation
@@ -28,9 +29,11 @@ class CarModel(models.Model):
         ('HATCH', 'Hatchback'),
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
-                               validators=[MaxValueValidator(2026),MinValueValidator(2005)]
-        )
+    year = models.IntegerField(default=2023, 
+                               validators=[
+                                MaxValueValidator(2026),
+                                MinValueValidator(2005)
+                            ])
     top_speed = models.IntegerField(default=100,
         validators=[
             MaxValueValidator(400),
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,10 +1,5 @@
-from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
-from django.contrib import messages
-from datetime import datetime
-
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
 import logging
@@ -16,7 +11,7 @@ from .models import CarMake, CarModel
 from .restapis import get_request, analyze_review_sentiments, post_review
 
 # Get an instance of a logger
-logger=logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 
 # Create a `login_request` view to handle sign in request
@@ -46,8 +41,6 @@ def logout_request(request):
 # Create a `registration` view to handle sign up request
 @csrf_exempt
 def registration(request):
-    context = {}
-
     # Load JSON data from the request body
     data = json.loads(request.body)
     username = data['userName']
@@ -56,26 +49,28 @@ def registration(request):
     last_name = data['lastName']
     email = data['email']
     username_exist = False
-    email_exist = False
     try:
         # Check if user already exists
         User.objects.get(username=username)
         username_exist = True
-    except:
+    except Exception as e:
         # If not, simply log this is a new user
         logger.debug("{} is new user".format(username))
 
     # If it is a new user
     if not username_exist:
         # Create user in auth_user table
-        user = User.objects.create_user(username=username, first_name=first_name,
-                                        last_name=last_name, password=password, email=email)
+        user = User.objects.create_user(username=username,
+                                        first_name=first_name,
+                                        last_name=last_name,
+                                        password=password,
+                                        email=email)
         # Login the user and redirect to list page
         login(request, user)
-        data = {"userName" : username, "status" : "Authenticated"}
+        data = {"userName": username, "status": "Authenticated"}
         return JsonResponse(data)
-    else :
-        data = {"userName" : username, "error" : "Already Registered"}
+    else:
+        data = {"userName": username, "error": "Already Registered"}
         return JsonResponse(data)
 
 
@@ -87,7 +82,8 @@ def get_cars(request):
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+        cars.append({"CarModel": car_model.name,
+                     "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
 
 
@@ -117,18 +113,19 @@ def get_dealer_reviews(request, dealer_id):
             response = analyze_review_sentiments(review_detail['review'])
             print(response)
             review_detail['sentiment'] = response['sentiment']
-        return JsonResponse({"status": 200,"reviews": reviews})
+        return JsonResponse({"status": 200, "reviews": reviews})
     else:
-        return JsonResponse({"status": 400,"message": "Bad Request"})
+        return JsonResponse({"status": 400, "message": "Bad Request"})
 
 
 def add_review(request):
-    if (request.user.is_anonymous == False):
+    if (request.user.is_anonymous is False):
         data = json.loads(request.body)
         try:
             response = post_review(data)
             return JsonResponse({"status": 200})
-        except:
-            return JsonResponse({"status": 401,"message": "Error in posting review"})
+        except Exception as e:
+            return JsonResponse({"status": 401,
+                                 "message": "Error in posting review"})
     else:
-        return JsonResponse({"status": 403,"message": "Unauthorized"})
+        return JsonResponse({"status": 403, "message": "Unauthorized"})
