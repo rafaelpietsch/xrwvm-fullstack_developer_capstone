e99fd145fceb656de46560e9a7fd47ca78250a20
rafael Pietsch <rafaelpietsch@outlook.com>
2025-09-17 20:19:31 -0400

    Corrected lint messages

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -2,22 +2,22 @@ from django.db import models
 from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     country = models.TextField()
     foundedYear = models.IntegerField(default=2025,
-        validators=[
-            MaxValueValidator(2026),
-            MinValueValidator(1900)
-        ])
+        validators=[MaxValueValidator(2026),MinValueValidator(1900)]
+        )
 
     def __str__(self):
         return self.name  # Return the name as the string representation
 
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -29,10 +29,8 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-        validators=[
-            MaxValueValidator(2026),
-            MinValueValidator(2005)
-        ])
+        validators=[MaxValueValidator(2026),MinValueValidator(2005)]
+        )
     top_speed = models.IntegerField(default=100,
         validators=[
             MaxValueValidator(400),
